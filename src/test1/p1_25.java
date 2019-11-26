92a4fc66864a635b0ea83ac142d4f0df9823aacf
jisoo <ske050588@gmail.com>
2019-11-26 21:25:44 +0900

    TimeComplexity

--- src/test1/p1_25.java
+++ src/test1/p1_25.java
@@ -4,7 +4,7 @@ public class p1_25 {
 	public static void t(int num) {
 	if (num>0) {
 		t(num/2);
-		System.out.print(num%2);
+		System.out.print(num%2); //2로 난눈 나머지가 프린트됨
 	}
 	}
 	public static void main(String[] args) {
